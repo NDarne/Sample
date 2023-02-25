8b7310df6eec354604058e500f9510bb052eddff
Krushna K9 <kudwant09@gmail.com>
2023-02-25 17:35:09 +0530

    new class added by k9

---
+++ SampleForGithub/src/test/java/com/Sampletest1/TestK9_A.java
@@ -0,0 +1,11 @@
+package com.Sampletest1;
+
+public class TestK9_A {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("pura project add karaycha hota");
+
+	}
+
+}
